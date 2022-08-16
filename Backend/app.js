b07c00027617a1fa5087963e73bac1899fd28a2f
Anton <94136841+kodareAnton@users.noreply.github.com>
2022-08-16 13:43:14 +0200

    iii

--- Backend/app.js
+++ Backend/app.js
@@ -27,6 +27,8 @@ io.on('connection', function(socket){
     });
         socket.on('chat message', function(msg){
             console.log('msg', msg);
+            io.emit('chat message', msg)
+        
         })
 
 });
